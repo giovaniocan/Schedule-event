880d2f8928bf04806e8b50f1462cf4d0207b27e4
giovani <giovaniocan@gmail.com>
2023-07-25 16:17:23 -0300

    added statubar css

--- App.tsx
+++ App.tsx
@@ -1,7 +1,15 @@
+import { StatusBar } from "react-native";
 import {Home} from "./src/screens/Home/Index";
 
 export default function App() {
   return(
-   <Home />
+   <>
+   <StatusBar 
+     barStyle="light-content"
+     backgroundColor="transparent" // change the background image
+     translucent // deixa a barra de status na tela, ela nao tem um nitida diferenca entre o aplicativo e a barra de status, faz elas ficarem juntas**//
+    />
+    <Home />
+   </>
   )
 }
